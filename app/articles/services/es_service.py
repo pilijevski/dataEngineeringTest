8f5cd50d5158bdff834f645d7c4075990469ffa6
pilijevski <petar.ilijevski@gmail.com>
2022-09-23 09:50:09 +0200

    Fixed issues with config and requirements

--- app/articles/services/es_service.py
+++ app/articles/services/es_service.py
@@ -4,12 +4,13 @@ from elasticsearch import Elasticsearch
 from tenacity import retry, stop_after_attempt, wait_fixed
 
 
+# > This class is used to connect to an Elasticsearch cluster and perform various operations on it
 class ElasticsearchService:
-    def __init__(self, host: str, port: int, username: str, password: str):
+    def __init__(self, host: str, port: int, username: str, password: str, scheme: str):
         self.host = host
         self.port = port
 
-        self.es = Elasticsearch([{'scheme': 'http', 'host': host, 'port': 9200}],
+        self.es = Elasticsearch([{'scheme': scheme, 'host': host, 'port': 9200}],
                                 basic_auth=(username, password))
         self.connect()
 
@@ -17,6 +18,10 @@ class ElasticsearchService:
            stop=stop_after_attempt(5),
            wait=wait_fixed(5))
     def connect(self):
+        """
+        Verifies the connection to Elasticsearch.
+        If the Elasticsearch endpoint is unreachable, log an error and raise a ConnectionError
+        """
         if not self.es.ping(human=True):
             logging.error(f"Elasticsearch endpoint {self.host}:{self.port} is unreachable.")
             raise ConnectionError
--- app/configs/config.py
+++ app/configs/config.py
@@ -1,8 +1,10 @@
+import os
+
 from config import Config
 
 
 class ConfigProvider:
-    def __init__(self,cfg_path="./app_config.cfg"):
+    def __init__(self, cfg_path="/".join([os.path.split(os.path.realpath(__file__))[0], "app_config.cfg"])):
         self.cfg = Config(str(cfg_path))
 
     def provide_elasticsearch(self):
